35470fed0c1765fc40b8765189785b7358e1ae97
Abdusselam <alidroid696@gmail.co>
2023-02-03 12:36:33 +0300

    Hive with flutter_adapter_type_generator

--- lib/model/user_settings.dart
+++ lib/model/user_settings.dart
@@ -1,10 +1,18 @@
+import 'package:hive/hive.dart';
 import '../enums/sex.dart';
+part 'user_settings.g.dart';
 
+@HiveType(typeId: 0)
 class UserSettingsModel {
+  @HiveField(0)
   final String userName;
+  @HiveField(1)
   final Sex sex;
+  @HiveField(2)
   final List<String> color;
+  @HiveField(3)
   final bool darkTheme;
+  @HiveField(4)
   final bool light;
 
   UserSettingsModel(
@@ -16,6 +24,10 @@ class UserSettingsModel {
 
   @override
   String toString() {
-    return 'UserSettingsModel{userName: $userName, sex: $sex, color: $color, darkTheme: $darkTheme, light: $light}';
+    return 'UserSettingsModel{userName: '
+        '$userName,'
+        ' sex: $sex, color: $color, '
+        'darkTheme: $darkTheme,'
+        ' light: $light}';
   }
 }
